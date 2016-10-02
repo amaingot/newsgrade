c979db214e92902e728f55bb1a9d583df855e685
Alex Maingot <amaingot@spredfast.com>
2016-10-01 22:01:26 -0500

    many random tests :)

--- db/populateSources.py
+++ db/populateSources.py
@@ -1,11 +1,24 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 import newspaper
+import settings
 import sys
+import MySQLdb as mdb
+
+con = mdb.connect(settings.db_endpoint, settings.db_username,  
+                  settings.db_password, settings.db_name)
 
 for source in newspaper.popular_urls():
     try: 
         soup = BeautifulSoup(urllib2.urlopen(source))
         print source + " - " + soup.title.string
+        cur = con.cursor()
+
+        cmd = str('INSERT INTO newsgrade.sources', 
+            '(name, url, last_fetched, last_error, source_type, source_subtype, metadata)',
+            'VALUES (', soup.title.string, ', ', source, ', NULL, NULL, "news", NULL,NULL);')
+        cur.execute(cmd)
+        print "success!"
+        exit()
     except:
         print "Error fetching ", source, "\n --> ", sys.exc_info()[0]
--- schedule/schedule.py
+++ schedule/schedule.py
@@ -2,6 +2,19 @@ import time
 import boto.sqs
 from boto.sqs.message import Message
 from datetime import datetime
+import settings
+
+
+sqs = boto.sqs.connect_to_region(
+                settings.sqs_region,
+                aws_access_key_id=settings.aws_user_key,
+                aws_secret_access_key=settings.aws_user_secret)
+queue = sqs.get_queue(settings.sqs_name)
+
+
 
 while (true):
-    
+    # Query for news sources
+
+
+    # For each news source, queue 
